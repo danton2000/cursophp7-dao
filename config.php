89c94ba2c92e9510a2d9e8c7b60fc04ad9b58681
danton2000 <Dantoncampos@hotmail.com>
2020-03-30 08:55:24 -0300

    colocando comentario no config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-
+//Registra a função dada como implementação de __autoload()
 spl_autoload_register(function($class_name){
    
     $filename = "class".DIRECTORY_SEPARATOR.$class_name.".php";
